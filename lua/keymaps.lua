7b690ab3edb13aad1b4f7f9d21279f76fc254fa1
Melxorn <meletaus2019@gmail.com>
2025-01-26 16:56:37 +0000

    Create option, keymaps, colorscheme and lazy install

---
+++ lua/keymaps.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n","<space><space>x","<cmd>source %<CR>")
+vim.keymap.set("n","<space><space>s","<cmd>w<CR>")
+vim.keymap.set("n","<space><space>e","<cmd>Lexplore 30<CR>")
+vim.keymap.set("n","<space>er","<cmd>Explore 30<CR>")
